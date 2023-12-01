4705d54a0b8e19abebd1062b2125f392f681ad9a
Illia Semenov <doku_isv@icloud.com>
2023-12-01 11:55:53 +0100

    Registry, Login, Success page v.0.0.1

--- GitHub Projet/script.js
+++ GitHub Projet/script.js
@@ -9,3 +9,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
     burgerMenu.addEventListener("click", toggleMenu);
 });
+
+function register() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username && password) {
+        sessionStorage.setItem('username', username);
+        sessionStorage.setItem('password', password);
+        window.location.href = 'Success.html';
+    } else {
+        alert('Please fill in both username and password fields.');
+    }
+};
+
+function login() {
+    const enteredUsername = document.getElementById('username').value;
+    const enteredPassword = document.getElementById('password').value;
+
+    const storedUsername = sessionStorage.getItem('username');
+    const storedPassword = sessionStorage.getItem('password');
+
+    if (enteredUsername === storedUsername && enteredPassword === storedPassword) {
+        window.location.href = 'Home.html';
+    } else {
+        alert('Invalid username or password.');
+    }
+};
