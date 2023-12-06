aae22c85dfd9c65dfafbd89efe91c7fb36cc493a
Illia Semenov <doku_isv@icloud.com>
2023-12-06 23:23:58 +0100

    feat(Home Page): Add structure to Home Page

--- GitHub Projet/script.js
+++ GitHub Projet/script.js
@@ -10,6 +10,39 @@ document.addEventListener("DOMContentLoaded", function() {
     burgerMenu.addEventListener("click", toggleMenu);
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+function autoSlides() {
+    plusSlides(1);
+}
+
+setInterval(autoSlides, 6000);
+
 function register() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
